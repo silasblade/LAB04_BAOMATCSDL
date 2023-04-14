34b819632d0e1e89ba5ef255121d5c85c188e3bc
Vishagna <111635138+silasblade@users.noreply.github.com>
2023-04-14 18:59:46 +0700

    Main

--- LAB04_Nhom/LAB04_Nhom/Form1.Designer.cs
+++ LAB04_Nhom/LAB04_Nhom/Form1.Designer.cs
@@ -44,6 +44,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Đăng nhập";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -53,6 +54,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Thoát";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
--- LAB04_Nhom/LAB04_Nhom/Form1.cs
+++ LAB04_Nhom/LAB04_Nhom/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,5 +17,35 @@ namespace LAB04_Nhom
         {
             InitializeComponent();
         }
+        public static string manv;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 Form2 = new Form2();
+            string tb1 = textBox1.Text;
+            string tb2 = textBox2.Text;
+            string connectionString = "Data Source=VISHAGNA;Initial Catalog=QLSVNhom;Integrated Security=True";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(" Select * From dbo.NHANVIEN WHERE MATKHAU = HASHBYTES('SHA1', '" + tb2 + "') AND MANV = '" + tb1 + "'", connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                connection.Close();
+                manv = tb1;
+                Form2.Show();
+                this.Hide();
+            }
+            else
+                {
+                    connection.Close();
+                    MessageBox.Show("Không tồn tại");
+                }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
-}
+    }
+
